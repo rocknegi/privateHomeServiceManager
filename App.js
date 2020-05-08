cf32bfa87904f3e06d9221c28b93ca82cd344929
rohit singh negi <rocknegi53@gmail.com>
2020-05-08 11:54:02 +0530

    add react native vector icons

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { createAppContainer, createSwitchNavigator, SafeAreaView } from 'react-navigation'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
+Icon.loadFont()
 const Login = ({navigation})=>{
   return(
     <SafeAreaView>
@@ -18,6 +20,7 @@ const Dashboard = ({navigation})=>{
   return(
     <SafeAreaView>
       <Text>Dashboard</Text>
+      {myIcon}
       <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
         <Text>Go Back</Text>
       </TouchableOpacity>
