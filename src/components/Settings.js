32b1d3cec70cce9545a569df0cbbce92a0900b3d
unknown <rocknegi53@gmail.com>
2020-07-02 13:09:57 +0530

    fix desc in settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, SafeAreaView, Image, Linking } from 'react-native'
+import { View, Text, SafeAreaView, Image, Linking, ActivityIndicator, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
 import { styles } from './theme/theme'
-import { TouchableWithoutFeedback, TouchableOpacity } from 'react-native-gesture-handler';
 import firestore from '@react-native-firebase/firestore';
 import Modal from 'react-native-modal';
+import OrderSummary from './orderSummary';
 
 const Settings = ({ navigation }) => {
     const [data, setData] = useState([]);
     const [currentUser, setCurrentUser] = useState('');
     const [isModalVisible, setModalVisible] = useState(false);
     const [currentOrderData, setCurrentOrderData] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [loadingDesc, setLoadingDesc] = useState(true);
 
     const orders = firestore().collection('Managers');
 
@@ -25,14 +27,19 @@ const Settings = ({ navigation }) => {
         const phone = currentUser
         const str = phone.toString()
         let data = [];
-
-        orders.doc(str).collection('Orders').doc('order').onSnapshot(doc => {
+        orders.doc(str).collection('Orders').doc('order').get().then(doc => {
             data.push(({ ...doc.data(), id: doc.id }));
             setCurrentOrderData(data);
-            // setTimeout(()=>{
-            // setLoading(false)
-            // },2000)
-        })
+            orders.doc(str).get().then(doc => setTotal(doc.data().balance))
+            setLoadingDesc(false)
+        });
+        // orders.doc(str).collection('Orders').doc('order').onSnapshot(doc => {
+        //     data.push(({ ...doc.data(), id: doc.id }));
+        //     setCurrentOrderData(data);
+        //     // setTimeout(()=>{
+        //     // setLoading(false)
+        //     // },2000)
+        // })
         // .then(doc=>{
 
         // })
@@ -46,7 +53,7 @@ const Settings = ({ navigation }) => {
     useEffect(() => {
         return orders.
             // where('accepted','==',false).
-            orderBy("date","desc").
+            orderBy("date", "desc").
             onSnapshot((snapshot) => {
                 let data = [];
                 snapshot.forEach(doc => {
@@ -67,77 +74,50 @@ const Settings = ({ navigation }) => {
 
             </View>
             <Modal
-                    isVisible={isModalVisible}
-                    onModalShow={afterModalOpen}
-                    style={{
-                        justifyContent: 'center',
-                        // margin: 0,
-                    }}
-                    onBackdropPress={toggleModal}
-                    swipeDirection={['up', 'left', 'right', 'down']}
-                >
- <View style={{ backgroundColor: '#fafafa' }}>
-                            {currentOrderData.map(item => (
-                                <View key={item.id} style={{ alignItems: 'center',margin:10 }}>
-                                <View style={{flexDirection:'row'}}>
-                                    <Text style={[styles.text,{fontSize:20,flex:1}]}>Balance</Text>
-                                    <Text style={[styles.text,{fontSize:20,flex:1}]}>{item.balance}</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row'}}>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>Champagne</Text>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.champagne}</Text>
-                                </View>
-                                <View style={{flexDirection:'row'}}>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>Champagne Glass</Text>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.champagneGlass}</Text>
-                                </View>
-                                {/* <View style={{flexDirection:'row'}}>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>Delivery Time</Text>
-                                <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.deliveryTime}</Text>
-                                    </View> */}
-                                    <View style={{flexDirection:'row'}}>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>Hotess</Text>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.hotess}</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row'}}>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>Time</Text>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.time} hrs</Text>
-                                    </View>
-                                    <View style={{flexDirection:'row'}}>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>Total</Text>
-                                    <Text  style={[styles.text,{fontSize:20,flex:1}]}>{item.total}</Text>
-                                    </View>
-                                </View>
-                            ))}
-                            {/* <TouchableOpacity onPress={toggleModal} style={[styles.button, { margin: 10,padding:10 }]}>
-                                <Text style={styles.text}>
-                                    close
+                isVisible={isModalVisible}
+                onModalShow={afterModalOpen}
+                style={{
+                    justifyContent: 'center',
+                    // margin: 0,
+                }}
+                onBackdropPress={toggleModal}
+                swipeDirection={['up', 'down']}
+                propagateSwipe={true}
+                useNativeDriver={true}
+            >
+                {loadingDesc ?
+                    <ActivityIndicator size="large" animating={true} />
+                    : <View style={{ backgroundColor: '#fafafa' }}>
+                        <OrderSummary items={currentOrderData} total={total} />
+                        <TouchableOpacity onPress={toggleModal} style={[styles.button, { margin: 10, padding: 10 }]}>
+                            <Text style={styles.text}>
+                                CLose
                             </Text>
-                            </TouchableOpacity> */}
-                        </View>
-                </Modal>
+                        </TouchableOpacity>
+                    </View>}
+            </Modal>
             <Text style={styles.heading}>Orders</Text>
-                <View style={styles.table}>
-                    <Text style={{flex:0.5,textAlign:'center'}}>O.Nr.</Text>
-                    <Text style={{flex:0.7,textAlign:'center'}}>Desc</Text>
-                    <Text style={{flex:0.8,textAlign:'center'}}>TM</Text>
-                    <Text style={{flex:0.5,textAlign:'center'}}>SRV</Text>
-                    <Text style={{flexGrow:1,flex:1,textAlign:'center'}}>Acc</Text>
-                    <Text style={{flex:1,textAlign:'center',right:5}}>Date</Text>
-                    <Text style={{flex:0,textAlign:'center',right:1}}>LOC</Text>
-                </View>
+            <View style={styles.table}>
+                <Text style={{ flex: 0.5, textAlign: 'center' }}>O.Nr.</Text>
+                <Text style={{ flex: 0.7, textAlign: 'center' }}>Desc</Text>
+                <Text style={{ flex: 0.8, textAlign: 'center' }}>TM</Text>
+                <Text style={{ flex: 0.5, textAlign: 'center' }}>SRV</Text>
+                <Text style={{ flexGrow: 1, flex: 1, textAlign: 'center' }}>Acc</Text>
+                <Text style={{ flex: 1, textAlign: 'center', right: 5 }}>Date</Text>
+                <Text style={{ flex: 0, textAlign: 'center', right: 1 }}>LOC</Text>
+            </View>
             {data.map(item => (
                 <View key={item.orderNo} style={styles.table}>
                     <Text style={{ flex: 0.5, textAlign: 'center' }}>{item.orderNo}</Text>
-                    <Text 
-                    onPress={() => openOrderModal(item.phoneNo)}
-                     style={{ flex: 0.7, textAlign: 'center' }}>Desc</Text>
+                    <Text
+                        onPress={() => openOrderModal(item.phoneNo)}
+                        style={{ flex: 0.7, textAlign: 'center' }}>Desc</Text>
                     <Text style={{ flex: 0.8, textAlign: 'center' }}>{item.deliveryTime}</Text>
                     <Text style={{ flex: 0.5, textAlign: 'center' }}>{item.serviceDuration}</Text>
                     <Text style={{ flexGrow: 1, flex: 1, textAlign: 'center' }}>{item.name}</Text>
                     <Text style={{ flex: 1, textAlign: 'center', right: 5 }}>{item.date}</Text>
                     <TouchableOpacity onPress={() => Linking.openURL(`https://www.google.com/maps/place/38.915645,-77.220796`)}>
-                        <Text style={{ flex: 0, textAlign: 'center', right: 1}}>LOC</Text>
+                        <Text style={{ flex: 0, textAlign: 'center', right: 1 }}>LOC</Text>
                     </TouchableOpacity>
                 </View>
             ))}
